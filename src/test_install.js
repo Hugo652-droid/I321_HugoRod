8f677990960814a885d547e254ee00cc9511b0f7
Hugo Rod <pb72yvp@eduvaud.ch>
2025-08-22 08:57:31 +0200

    test install express

--- src/test_install.js
+++ src/test_install.js
@@ -1,8 +1,15 @@
+/*
+* Auteur : Hugo Rod
+* Description : Test de l'installation
+* Date : 22.08.2025
+* */
 
+import express from 'express';
 
+// Test js
 function test(numbers) {
     let product = 1
-    for (i = 0; i < numbers.length; i++) {
+    for (let i = 0; i < numbers.length; i++) {
         product *= numbers[i]
     }
     return product
@@ -11,3 +18,11 @@ function test(numbers) {
 let numbers = [1,2,3,7,1]
 
 console.log(test(numbers))
+
+// Test express (https://expressjs.com/en/starter/hello-world.html)
+const app = express();
+const port = 3000;
+
+app.get('/', (req, res) => {res.send('Hello World!')});
+
+app.listen(port, () => console.log(`Listening on port ${port}`));
